0c8cb92735214586722d991b05f566ca9c4f1c72
Luis Ocasio <luisocasio03@gmail.com>
2019-11-18 10:17:18 -0500

    remove table

--- data/migrations/20191116191008_bootstrapdb.js
+++ data/migrations/20191116191008_bootstrapdb.js
@@ -1,25 +1,15 @@
 require("dotenv").config();
 
 exports.up = function(knex) {
-  return knex.schema
+  return knex.schema.createTable("users", tbl => {
+    tbl.increments();
 
-    .createTable("users", tbl => {
-      tbl.increments();
-
-      tbl
-        .string("username", 255)
-        .notNullable()
-        .unique();
-      tbl.string("password", 255).notNullable();
-    })
-
-    .createTable("photos", tbl => {
-      tbl.increments();
-
-      tbl.string("title", 255).notNullable();
-      tbl.string("url", 255).notNullable();
-      tbl.increments();
-    });
+    tbl
+      .string("username", 255)
+      .notNullable()
+      .unique();
+    tbl.string("password", 255).notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
